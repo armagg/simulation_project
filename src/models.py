1052246f732919ceb247f764c011cce2ef4dc391
omid <omid.mesgarha@gmail.com>
2021-07-14 00:50:03 +0430

    add interfaces for models

---
+++ src/models.py
@@ -0,0 +1,54 @@
+class Costumer:
+    def __init__(self) -> None:
+        pass
+
+    
+class PriorityQueue:
+    def __init__(self) -> None:
+        pass
+
+    def add_to_queue(object, priority):
+        pass
+    
+    def get_all_objects():
+        pass
+
+    def remove_object(object):
+        pass
+
+
+class Server:
+    '''
+    An abstract class for serving costumers with priority queues 
+    
+    '''
+
+    def __init__(self) -> None:
+        pass
+
+
+class Reception(s):
+    def __init__(self) -> None:
+        pass
+
+    def serve():
+        pass
+
+    def add_to_queue():
+        pass
+
+
+class Store:
+    def __init__(self) -> None:
+        pass
+    
+    def serve():
+        pass
+
+
+
+class Chef:
+    def __init__(self) -> None:
+        pass
+
+
