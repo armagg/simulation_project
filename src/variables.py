592247340fb308efadd50fb1dc883996a15d2d6f
omid <omid.mesgarha@gmail.com>
2021-07-15 20:00:09 +0430

    poisson and exponential added with numpy

--- src/variables.py
+++ src/variables.py
@@ -1,9 +1,29 @@
+import random, math
+import numpy as np
+
 #TODO: get variable and distributions with a function call 
 
-
 def get_poisson_variable(miu):
-    pass
+    value = np.random.poisson(miu)
+    return math.floor(value)
 
-def get_exponential_variable(landa):
-    pass
+def get_exponential_variable(alpha):
+    value =  np.random.exponential(alpha)
+    return math.floor(value)
 
+
+def generate_costumer_priority():
+        rand = random.randint(0, 99)
+        if rand < 50 : 
+            return 0
+        elif rand < 70:
+            return 1
+        elif rand < 85:
+            return 2
+        elif rand < 95:
+             return 3
+        else:
+            return 4
+
+def init_data():
+    pass
